7ea3bd1e5560fb1cb0975bf6dfe8e1515174b374
Georgi Georgiev <gogo4ds@gmail.com>
2016-08-09 22:00:51 +0300

    Enabled Migrations and Implemented Seed Method

--- WebStepBlog/Global.asax.cs
+++ WebStepBlog/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebStepBlog.Models;
 
 namespace WebStepBlog
 {
@@ -12,6 +14,12 @@ namespace WebStepBlog
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<
+                    ApplicationDbContext, 
+                    Migrations.Configuration>()
+                );
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
