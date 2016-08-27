35bf4fcebf8e5ff5cd032030d6daec04d67201f1
u-ashikov <u.ashikov@abv.bg>
2016-08-28 01:40:08 +0300

    Tag design

--- WebStepBlog/Controllers/PostsController.cs
+++ WebStepBlog/Controllers/PostsController.cs
@@ -178,7 +178,7 @@ namespace WebStepBlog.Controllers
 
         private Post GetPost(int? id)
         {
-            return id.HasValue ? db.Posts.Include(p => p.Author).Where(x => x.Id == id).First() : new Post { Id = -1 };
+            return id.HasValue ? db.Posts.Include(p => p.Author).Include(t=>t.Tags).Where(x => x.Id == id).First() : new Post { Id = -1 };
         }
 
 
