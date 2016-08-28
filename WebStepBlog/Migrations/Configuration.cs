0f0df0d05f3232fdad9575d6b7332bdec4f5b18d
ytlys <natlyda@abv.bg>
2016-08-28 18:09:13 +0300

    migrations
    content

--- WebStepBlog/Migrations/Configuration.cs
+++ WebStepBlog/Migrations/Configuration.cs
@@ -22,76 +22,67 @@ namespace WebStepBlog.Migrations
                 // If the database is empty, populate sample data in it
 
                 CreateUser(context, "admin@gmail.com", "123", "System Administrator");
-                CreateUser(context, "pesho@gmail.com", "123", "Peter Ivanov");
-                CreateUser(context, "merry@gmail.com", "123", "Maria Petrova");
-                CreateUser(context, "geshu@gmail.com", "123", "George Petrov");
+                CreateUser(context, "pesho@gmail.com", "123", "Peter Parker");
+                CreateUser(context, "merry@gmail.com", "123", "Mariah Smith");
+                CreateUser(context, "geshu@gmail.com", "123", "George Kostov");
 
                 CreateRole(context, "Administrators");
                 AddUserToRole(context, "admin@gmail.com", "Administrators");
 
                 CreatePost(context,
-                    title: "Work Begins on HTML5.1",
-                    body: @"<p>The World Wide Web Consortium (W3C) has begun work on <b>HTML5.1</b>, and this time it is handling the creation of the standard a little differently. The specification has its <b><a href=""https://w3c.github.io/html/"">own GitHub project</a></b> where anyone can see what is happening and propose changes.</p>
-                    <p>The organization says the goal for the new specification is ""to <b>match reality better</b>, to make the specification as clear as possible to readers, and of course to make it possible for all stakeholders to propose improvements, and understand what makes changes to HTML successful.""</p>
-                    <p>Creating HTML5 took years, but W3C hopes using GitHub will speed up the process this time around. It plans to release a candidate recommendation for HTML5.1 by <b>June</b> and a full recommendation in <b>September</b>.</p>",
+                    title: "2002 BMW 3-Series 325CI",
+                    body: @"FLORIDA CONDO CAR
+                           2002 BMW 325CI with 108,799 miles FLY IN AND DRIVE HOME, OR STAY IN SUNNY FLORIDA FOR THE WINTER, WITH A PRISTINE BMW CONVERTIBLE TO ENJOY THE SUN AND SAND.
+                            THIS IS A LOCAL NAPLES FLORIDA, GULF SHORE BOULEVARD CONDO CAR.
+                            EXTERIOR IS SILVER (STAYS COOLER IN THE SUN) AND COMPLEMENTED BY A GRAY LEATHER INTERIOR,  BLACK POWER CONVERTIBLE TOP
+                            RECENT SERVICE, LIKE NEW BLACK POWER TOP, TOOL KIT, BOOK, SUPER CLEAN,ITS ALWAYS NICE TO FIND A LOW MILAGE FLORIDA CAR.
+                            US $8,499.00",
                     date: new DateTime(2016, 03, 27, 17, 53, 48),
-                    authorUsername: "merry@gmail.com"
+                    authorUsername: "pesho@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "Windows 10 Preview with Bash Support Now Available",
-                    body: @"<p>Microsoft has released a new <b>Windows 10 Insider Preview</b> that includes native support for <b>Bash running on Ubuntu Linux</b>. The company first announced the new feature at last week''s Build development conference, and it was one of the biggest stories of the event. The current process for installing Bash is a little complication, but Microsoft has a blog post that explains how the process works.</p>
-                    <p>The preview build also includes <b>Cortana</b> upgrades, extensions support, the new <b>Skype</b> Universal Windows Platform app and some interface improvements.</p>",
+                    title: "1987 BMW M6",
+                    body: @"This BMW E24 M6 is not only a great investment vehicle but it is the perfect balance of classic design and elegant performance, No vintage BMW offers a combination like a e24 M6, there going to become un-affordable soon enough. With only 1632 of these built how many have not been wrecked, neglected, restored? How many are all original with original paint and in near perfect condition? There only original once. There are very few of these 6ers that have been maintained like this one and can still truly be driven daily and enjoyed while increasing in value at the same time. This 6er looks like a time capsule, shows like a 10k mile car. All the original nickel plating from top to bottom shines like the day it left the showroom... there is no comparable to this m6.
+                            This classic M Series has been cared for extensively, Everything works! as if it were new… the previous owner has kept and documented this car in detail as you will see with the accompanied receipts and records that are all included with the purchase. All original VIN Tags are pictured, hood, door jams, trunk etc.
+                            Also Includes the original tool kit, with all original tools. Everything on this M6 is in working order! The A/C blows Ice Cold, and all interior components are in working order! Engine and transmission run flawlessly. This is a no issue 6er, pulls nice through every gear. Starts and Runs Smooth Every time. Undercarriage is at Mint as it comes. Rust Free BMW. This M6 has 132K Miles but drives like new, it was well taken care of and shows.
+                            US $39,500.00",
                     date: new DateTime(2016, 05, 11, 08, 22, 03),
                     authorUsername: "merry@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "Atom Text Editor Gets New Windows Features",
-                    body: @"<p>GitHub has released <b>Atom 1.7</b>, and the updated version of the text editor offers improvements for Windows developers. Specifically, it is now easier to build in Visual Studio, and it now supports the Appveyor CI continuous integration service for Windows.</p>
-                    <p>Other new features include improved tab switching, tree view and crash recovery. GitHub noted, ""Crashes are nobody''s idea of fun, but in case Atom does crash on you, it periodically saves your editor state. After relaunching Atom after a crash, you should find all your work saved and ready to go.""</p>
-                    <p>GitHub has also released a beta preview of Atom 1.8.</p>",
+                    title: "1979 BMW 3-Series 320i",
+                    body: @"a 350 small block and chevy tranny. vehicle runs and drives. Vehicle comes with BC serious struts that are adjustable. Two 3inch flow master super 10 and a bm shifter. Solid body and very fun car. Asking 2800.00 
+                            US $2,800.00",
                     date: new DateTime(2016, 03, 27, 17, 53, 48),
-                    authorUsername: "merry@gmail.com"
+                    authorUsername: "geshu@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "SoftUni 3.0 Just Launched",
-                    body: @"<p>The <b>Software University (SoftUni)</b> launched a new training methodology and training program for software engineers in Sofia.</p>
-                    <p>It is a big step ahead. Now SoftUni offers several professions:</p>
-                    <ul>
-                      <li>PHP Developer</li>
-                      <li>JavaScript Developer</li>
-                      <li>C# Web Developer</li>
-                      <li>Java Web Developer</li>
-                    </ul>",
+                    title: "2015 Audi A3",
+                    body: @"UP FOR SALE IS A BEAUTIFUL 2015 AUDI A3 1.8T BLUE ON BLACK LEATHER INTERIOR WITH ONLY 12K ORIGINAL MILES!!!!!!
+                            WE HAVE RECENTLY PURCHASED THIS A3 FROM AN INSURANCE COMPANY WITH NO DAMAGE!!!!! AS YOU SEE THE VEHICLE PICTURED BELOW... IS IN THE SAME STATE AS WE PURCHASED IT.
+                            THIS AUDI A3 HAD MINOR PREVIOUS COLLISION DAMAGE TO THE FRONT WHICH THE INSURANCE COMPANY HAD PROFESSIONALLY REPAIRED PRIOR TO SALE. THERE ARE NO SIGNS OF ANY PREVIOUS WORK!!!! PAINT MATCHED 100%!!!! DRIVES EXCELLENT!!!! NO WIERD NOISES!!! NO VIBRATIONS!!! DRIVES STRAIGHT AND HANDLES GREAT!!! NO KNOWN MECHANICAL ISSUES!!! NO LIGHTS ON DASH!!!
+                            THE CAR IS IN GREAT CONDITION INSIDE AND OUT AS YOU MAY SEE ON THE 70+ PICTURES LISTED BELOW!!!
+                            EQUIPT WITH: 1.8T ENGINE, S-TRONIC TRANSMISSION, XENON LED HEADLIGHTS, NAVIGATION, KEYLESS GO & ENTRY, SATELLITE RADIO, BLUETOOTH, DUAL ZONE CLIMATE CONTROL, MULTI-FUNCTION STEERING WHEEL CONTROLS, SUNROOF, CRUISE, POWER EVERYTHING, LEATHER SEATS, AND MUCH MUCH MORE....
+                            THE INSURANCE COMPANY VALUED THIS AUDI A3 AT $26,698",
                     date: new DateTime(2016, 02, 18, 22, 14, 38),
                     authorUsername: "pesho@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "Git 2.8 Adds Security and Productivity Features",
-                    body: @"<p>Version 2.8 of the open-source distributed version-control system Git has been released. The new edition provides a variety of new features, bugfixes and other improvements.</p>
-                    <p>According to GitHub, the most notable new features include:</p>
-                    <ul>
-                    <li><strong>Parallel fetches of submodules:</strong> “Using ‘git submodules,’ one Git repository can include other Git repositories as subdirectories. This can be a useful way to include libraries or other external dependencies into your main project. The top-level repository specifies which submodules it wants to include, and which version of each submodule,” wrote Jeff King, a Git team member, in a <a href=""https://github.com/blog/2131-git-2-8-has-been-released"">blog post</a>. According to him, if users have multiple submodules, fetches can be time-consuming. The latest release allows users to fetch from multiple submodules in parallel.</li>
-                    <li><strong>Don’t guess my identity: </strong>Instead of using one e-mail address for all of a user’s open-source projects, they can now tell Git what user name and e-mail they want to use before they commit.</li>
-                    <li><strong>Convergences with Git for Windows:</strong> The Git team has been working on making Git as easy to work with on Windows as it is on Linux and OS X. The latest release includes Git commands rewritten in C; Windows-specific changes from the Git for Windows project; and the ability to accept both LF and CRLF line endings. “This continuing effort will make it easier to keep the functionality of Git in sync across platforms as new features are added,” King wrote.</li>
-                    <li><strong>Security fixes: </strong>Git 2.8 addresses the vulnerability CVE-2016-2324. There have not been any reported exploits, but the vulnerability could execute arbitrary code when cloning a malicious repository, according to King.</li>
-                    </ul>
-                    <p>Other features include the ability to turn off Git’s clean and smudge filters; the ability to see where a particular setting came from; the ability to easily diagnose end-of-line problems; the ability to see a remote repository’s default branch; and support for cloning via the rsync protocol has been dropped.</p>
-                    <p>The full release notes are available <a href=""https://github.com/git/git/blob/v2.8.0/Documentation/RelNotes/2.8.0.txt"">here</a>.</p>",
+                    title: "2008 Toyota Corolla No Reserve,Warranty,No Reserve",
+                    body: @"The 2008 Toyota Corolla comes in three different models: the base CE, sporty S, and better equipped LE. Each can be equipped with either a five-speed manual transmission or four-speed automatic. The 126-horsepower, 1.8L four-cylinder engine that powers all Corollas has variable valve timing with intelligence, which helps give it good response through the rev range without hampering fuel efficiency. With the manual transmission, the Corolla can accelerate from zero to 60 mph in just under nine seconds, according to Toyota, yet it achieves EPA fuel economy ratings of 28 mpg city, 35 mpg highway.The Corolla has a roomy interior and comfortable ride, yet a stiff structure and variable-assist rack-and-pinion steering give it a nimble feel. The back seat is split 60/40 and folds forward to expand the 13.6-cubic-foot trunk.Side-bolster front-seat air bags and full-length curtain air bags are available as an option across the line, as are anti-lock brakes, while electronic stability control is available only on LE and S models with the optional automatic transmission. Models equipped with the stability control system and anti-lock brakes also get brake assist, which helps apply full brake force in emergency braking.Standard equipment on the base CE model includes air conditioning, tilt steering, a height-adjustable driver's seat, a trunk lamp, power mirrors, and a CD sound system with four speakers. The S model adds a sportier look, with smoked headlamp lenses, integrated front fog lamps, under-body spoilers, rocker panel extensions, and rear mudguards, and it also upgrades to power locks, map lights, and a six-speaker sound system. The LE doesn't get all the sporty appearance cues of the S but instead has remote keyless entry, power windows, and woodgrain trim. Other major options include a power moonroof, alloy wheels, and an eight-speaker JBL sound system with six-disc changer
+                            us $3,100.00",
                     date: new DateTime(2016, 04, 11, 19, 02, 05),
                     authorUsername: "geshu@gmail.com"
                 );
 
                 CreatePost(context,
-                    title: "Rogue Wave Updates Zend Framework",
-                    body: @"<p>Rogue Wave is updating its open-source framework for developing Web applications and services. According to the company, this is the first major release in four years. Zend Framework 3 features support for PHP 7, middleware runtime and performance enhancements.</p>
-                    <p>The newly released support for PHP 7 aims to simplify how developers create, debug, monitor and deploy modern Web and mobile apps in PHP 7. “This is an exciting time to be a PHP developer,” said Zeev Suraski, cofounder of Zend and CTO of Rogue Wave. “With Zend Framework 3, we’re continuing our quest to make creating PHP applications simpler, more accessible and faster.”</p>
-                    <p>In addition, version 3 of the framework features an architectural structure that allows developers to use components within Zend Framework apps or any other framework in order to reduce dependencies, and to enable reuse within the PHP ecosystem.</p>
-                    <p>Another key update to the solution is a new middleware runtime. Expressive is designed to focus on simplicity and interoperability, and it enables developers to customize their solutions.</p>
-                    <p>“I’m extremely proud of the work we’ve done with Expressive,” said Matthew Weier O’Phinney, principal engineer and Zend Framework project lead at Rogue Wave. “Expressive signals the future of PHP applications, composed of layered, single-purpose PSR-7 middleware.”</p>",
+                    title: "2010 Toyota Tundra SR5 Double Cab",
+                    body: @"We are pleased to offer this 2010 Toyota Tundra SR5 Double Cab that is damaged as shown (please take a look at pictures for current damage). We can offer Domestic and International shipping arrangements, please take a look at the pictures for more details and don't pass up the opportunity to own this for a fraction of the original sales price as the listing can be removed any second due to the number of potential buyers!!!! This Vehicle has a SALVAGE TITLE and currently is not registered. The buyer will have to register it in his or her state of residence, which may or may not involve some extra steps compared to registering a clean title car. All California Buyer ONLY must pay 9% sales tax and will receive a Acquisition Bill Of Sale. We make no representations about repairability, availability of parts or costs of repairs.
+                            US $6,950.00",
                     date: new DateTime(2016, 06, 30, 17, 36, 52),
                     authorUsername: "merry@gmail.com"
                 );
